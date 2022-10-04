9b4e9e26ecb9161f9e98bf82a617ad78f010a6e0
Zheng-Xiang Ye <65601854+yehzx@users.noreply.github.com>
2022-10-04 09:14:32 +0800

    Complete full IAA_pathway based on KEGG

--- src/pathway.py
+++ src/pathway.py
@@ -31,16 +31,26 @@ class Enzyme():
         self.count = count
 
 
-PATHWAY_LIST = {1: PathwayNode("trp", [1, 3], True),
+PATHWAY_LIST = {1: PathwayNode("trp", [1, 3, 7, 9], True),
                 2: PathwayNode("iam_1", [2], False),
-                3: PathwayNode("indole-3-acetic-acid", [0], False),
-                4: PathwayNode("ipa_1", [4], False),
-                5: PathwayNode("ipa_2", [5], False)
+                3: PathwayNode("iaa", [0], False),
+                4: PathwayNode("ipa_1", [4, 6], False),
+                5: PathwayNode("ipa_2", [5], False),
+                6: PathwayNode("tam_1", [8], False),
+                7: PathwayNode("iaox", [10], False),
+                8: PathwayNode("ian_1", [11, 12], False)
                 }
 
 ENZYME_LIST = {0: None,
                1: Enzyme("trp_iam_1", 2),
-               2: Enzyme("iam_1_indole", 3),
+               2: Enzyme("iam_1_iaa", 3),
                3: Enzyme("trp_ipa_1", 4),
                4: Enzyme("ipa_1_2", 5),
-               5: Enzyme("ipa_2_indole", 3)}
+               5: Enzyme("ipa_2_iaa", 3),
+               6: Enzyme("ipa_1_iaa", 3),
+               7: Enzyme("trp_tam_1", 6),
+               8: Enzyme("tam_1_ipa_2", 5),
+               9: Enzyme("trp_iaox", 7),
+               10: Enzyme("iaox_ian_1", 8),
+               11: Enzyme("ian_1_iaa", 3),
+               12: Enzyme("ian_1_iam_1", 2)}
