aed0a4eea5cecce07e2d4614fee5b2f08cb3ed64
Zheng-Xiang Ye <65601854+yehzx@users.noreply.github.com>
2022-10-12 20:04:40 +0800

    WIP: merge module.py functions to main.py

--- main.py
+++ main.py
@@ -13,19 +13,91 @@ from src.parse_ncbi_xml import parse_blast
 from src.run_scripts import cpu_num, run_blast, run_prodigal
 from src.util import make_dir
 
+def parse_blast_(output_dir, input_folder):
+    file_list = glob.glob(os.path.join(input_folder, "*.xml"), recursive=True)
+    file_list = [file.replace("\\", "/") for file in file_list]
+    parse_blast_folder = os.path.join(output_dir, "tmp/parse_blast")
+    make_dir(parse_blast_folder)
+    print("Parse blastp result")
+    for filename in tqdm(file_list):
+        basename = os.path.basename(filename)
+        basename = re.sub(".xml", ".csv", basename)
+        output_name = os.path.join(parse_blast_folder, basename)
+        parse_blast(filename, output_name)
+    print("Done!")
+
+def find_best_blast_(output_dir, input_folder, criteria):
+    file_list = glob.glob(os.path.join(input_folder, "*.csv"), recursive=True)
+    file_list = [file.replace("\\", "/") for file in file_list]
+    best_blast_folder = os.path.join(output_dir, "tmp/best_blast")
+    make_dir(best_blast_folder)
+    print("Select the best blastp result based on the configuration")
+    for filename in tqdm(file_list):
+        basename = os.path.basename(filename)
+        output_name = os.path.join(best_blast_folder, basename)
+        find_best_blast(filename, output_name, criteria=criteria)
+    print("Done!")
+
+def match_enzyme_(output_dir, input_folder):
+    file_list = glob.glob(os.path.join(input_folder, "*.csv"), recursive=True)
+    file_list = [file.replace("\\", "/") for file in file_list]
+    enzyme_mapping_folder = os.path.join(output_dir, "result")
+    make_dir(enzyme_mapping_folder)
+    print("Match the best blastp result to enzyme pathway")
+    for filename in tqdm(file_list):
+        basename = os.path.basename(filename)
+        basename = re.search("(.*).csv", basename).group(1)
+        output_name = os.path.join(enzyme_mapping_folder, f"{basename}.txt")
+        print(f"\n--------{basename}--------")
+        run_match_enzyme(filename, output_name)
+    print("Done!")
+
+ROOT_DIR= os.path.dirname(__file__)
 
 def main():
+    # count total execution time
     time_start = time.time()
-    abs_dir = os.path.dirname(__file__)
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", type=str, help="input a file or directory path")
+    # without subcommand:
+    # a positional argument to the path of the data
+    parser.add_argument("datapath", type=str, help="input a file or directory path")
     # --debug not yet implemented
     parser.add_argument("--debug", action="store_true",
                         help="keep tmp folder if specified")
+    parser.set_defaults(func=main)
+    
+    # with subcommand: run individual module
+    subparser = parser.add_subparsers(help="module name")
+    prodigal_parser = subparser.add_parser("prodigal")
+    prodigal_parser.add_argument("input", help="[input_path]")
+    prodigal_parser.set_defaults(func=run_prodigal_module)
+
+    blast_parser = subparser.add_parser("blastp")
+    blast_parser.add_argument("input", nargs="*",
+                              help="[input_path] [optional:database_path]")
+    blast_parser.set_defaults(func=run_blast_module)
+
+    xml_parser = subparser.add_parser("parse_xml")
+    xml_parser.add_argument("input", help="[input_path]")
+    xml_parser.set_defaults(func=parse_blast_module)
+
+    best_blast_parser = subparser.add_parser("best_blast")
+    best_blast_parser.add_argument("input", nargs="*",
+                                   help="[input_path] [optional:criteria]")
+    best_blast_parser.set_defaults(func=find_best_blast_module)
+
+    match_enzyme_parser = subparser.add_parser("match_enzyme")
+    match_enzyme_parser.add_argument("input", help="[input_path]")
+    match_enzyme_parser.set_defaults(func=match_enzyme_module)
+
     args = parser.parse_args()
+    args.func(args)
+
+    # TODO: finish calling module funcs 
 
     # read config
-    with open(os.path.join(abs_dir, "config.toml"), "rb") as f:
+    with open(os.path.join(ROOT_DIR, "config.toml"), "rb") as f:
         config = tomli.load(f)
     
     # check blast database existence before running
@@ -38,8 +110,9 @@ def main():
         print("Blast database does not exist. Check config.toml before running.")
 
     # shell scripts
-    prodigal_folder = os.path.join(abs_dir, "tmp/prodigal")
-    blast_folder = os.path.join(abs_dir, "tmp/blast")
+    prodigal_folder = os.path.join(ROOT_DIR, "tmp/prodigal")
+    blast_folder = os.path.join(ROOT_DIR, "tmp/blast")
+    # check available threads
     cpus = cpu_num()
     # run prodigal gene prediction
     run_prodigal(args.file, prodigal_folder)
@@ -48,46 +121,20 @@ def main():
     
     # python functions
     # parse_blast
-    file_list = glob.glob(os.path.join(blast_folder, "*.xml"), recursive=True)
-    file_list = [file.replace("\\", "/") for file in file_list]
-    parse_blast_folder = os.path.join(abs_dir, "tmp/parse_blast")
-    make_dir(parse_blast_folder)
-    print("Parse blastp result")
-    for filename in tqdm(file_list):
-        basename = os.path.basename(filename)
-        basename = re.sub(".xml", ".csv", basename)
-        output_name = os.path.join(parse_blast_folder, basename)
-        parse_blast(filename, output_name)
-    print("Done!")
+    parse_blast_(output_dir=ROOT_DIR, input_folder=blast_folder)
 
     # find_best_blast
-    file_list = glob.glob(os.path.join(parse_blast_folder, "*.csv"), recursive=True)
-    file_list = [file.replace("\\", "/") for file in file_list]
-    best_blast_folder = os.path.join(abs_dir, "tmp/best_blast")
-    make_dir(best_blast_folder)
-    print("Select the best blastp result based on the configuration")
     # default: find highest bit-score (column: score)
     # options: score, evalue, identity_percentage, query_coverage
     criteria = config["criteria"]["column"]
-    for filename in tqdm(file_list):
-        basename = os.path.basename(filename)
-        output_name = os.path.join(best_blast_folder, basename)
-        find_best_blast(filename, output_name, criteria=criteria)
-    print("Done!")
+    find_best_blast(output_dir=ROOT_DIR,
+                    input_folder=os.path.join(ROOT_DIR, "tmp/parse_blast"),
+                    criteria=criteria)
 
     # match_enzyme
-    file_list = glob.glob(os.path.join(best_blast_folder, "*.csv"), recursive=True)
-    file_list = [file.replace("\\", "/") for file in file_list]
-    enzyme_mapping_folder = os.path.join(abs_dir, "result")
-    make_dir(enzyme_mapping_folder)
-    print("Match the best blastp result to enzyme pathway")
-    for filename in tqdm(file_list):
-        basename = os.path.basename(filename)
-        basename = re.search("(.*).csv", basename).group(1)
-        output_name = os.path.join(enzyme_mapping_folder, f"{basename}.txt")
-        print(f"\n--------{basename}--------")
-        run_match_enzyme(filename, output_name)
-    print("Done!")
+    match_enzyme_(output_dir=ROOT_DIR,
+                  input_folder=os.path.join(ROOT_DIR, "tmp/best_blast"))
+
     time_end = time.time()
     print(f"Elapsed time: {round(time_end - time_start, 2)}sec")
 
