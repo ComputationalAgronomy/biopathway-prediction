be9730c50961cf748fb4f9254fe92857221e61a8
Zheng-Xiang Ye <65601854+yehzx@users.noreply.github.com>
2022-11-08 18:53:26 +0800

    Run prodigal in parallel (EC2 tested)

--- src/run_prodigal.sh
+++ src/run_prodigal.sh
@@ -2,11 +2,18 @@
 tmpfolder="$2"
 cpus=$4
 mkdir -p $tmpfolder
+function check_prodigal_input () {
+    base_name=$(basename -s .fna "$1")
+    prodigal -i "$1" -o "$tmpfolder/$base_name.genes" -a "$tmpfolder/$base_name.faa"
+}
+export -f check_prodigal_input
+export tmpfolder
+
 echo "Start prodigal gene prediction"
 if [ -d "$1" ]; then
     filename=$(find "$1" -name "*.fna")
     filecount=$(echo "$filename" | wc -w)
-    echo "$filename" | xargs -n 1 -I {} -P $cpus prodial -i {} -o "$tmpfolder/$base_name.genes" -a "$tmpfolder/$base_name.faa" 2> /dev/null
+    echo "$filename" | xargs -n 1 -I {} -P $cpus bash -c 'check_prodigal_input {}'
     # for file in $filename
     # do
     #     count=$((count+1))
@@ -16,8 +23,11 @@ if [ -d "$1" ]; then
     # done
 elif [ -f "$1" ]; then
     base_name=$(basename -s .fna "$1")
-    prodigal -i "$1" -o "$tmpfolder/$base_name.genes" -a "$tmpfolder/$base_name.faa" 2> /dev/null 
+    prodigal -i "$1" -o "$tmpfolder/$base_name.genes" -a "$tmpfolder/$base_name.faa" 2> /dev/null
 else
     echo "Invalid input"
 fi
 echo "Finish prodigal gene prediction"
+
+unset check_prodigal_input
+unset tmpfolder
