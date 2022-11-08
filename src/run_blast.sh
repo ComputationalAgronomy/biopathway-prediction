b39a290070b10a24d917965685e3a8fcfd0f0d18
Zheng-Xiang Ye <65601854+yehzx@users.noreply.github.com>
2022-11-08 10:33:15 +0800

    Feature: run blast via  diamond

--- src/run_blast.sh
+++ src/run_blast.sh
@@ -14,11 +14,13 @@ if [ -d "$1" ]; then
         count=$((count+1))
         echo -ne "Progress: $count/$filecount\r"
         base_name=$(basename -s "$filetype" "$file")
-        cat "$file" | parallel --gnu --plain -j $cpus --block-size 10K --recstart '>' --pipe blastp -query - -db "$db" -num_threads 1 -evalue 1e-10 -num_alignments 3 -outfmt 5 >> "$tmpfolder/$base_name.xml" 2> /dev/null
+        diamond blastp -d ${db%.fasta} -q $file --outfmt 5 --xml-blord-format -o $tmpfolder/$base_name.xml
+        # cat "$file" | parallel --gnu --plain -j $cpus --block-size 10K --recstart '>' --pipe blastp -query - -db "$db" -num_threads 1 -evalue 1e-10 -num_alignments 3 -outfmt 5 >> "$tmpfolder/$base_name.xml" 2> /dev/null
     done
 elif [ -f "$1" ]; then
     base_name=$(basename -s "$filetype" "$1")
-    cat "$1" | parallel --gnu --plain -j $cpus --block-size 10K --recstart '>' --pipe blastp -query - -db "$db" -num_threads 1 -evalue 1e-10 -num_alignments 3 -outfmt 5 >> "$tmpfolder/$base_name.xml" 2> /dev/null
+    diamond blastp -d ${db%.fasta} -q $1 --outfmt 5 --xml-blord-format -o $tmpfolder/$base_name.xml
+    # cat "$1" | parallel --gnu --plain -j $cpus --block-size 10K --recstart '>' --pipe blastp -query - -db "$db" -num_threads 1 -evalue 1e-10 -num_alignments 3 -outfmt 5 >> "$tmpfolder/$base_name.xml" 2> /dev/null
 else
     echo "Invalid input"
 fi
