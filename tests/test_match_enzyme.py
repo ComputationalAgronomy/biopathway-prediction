4f0bde8b484b172f66febfd1ba4239c0194904d3
Zheng-Xiang Ye <65601854+yehzx@users.noreply.github.com>
2022-11-20 09:27:14 +0800

    Update test sets

--- tests/test_match_enzyme.py
+++ tests/test_match_enzyme.py
@@ -2,7 +2,8 @@ import os
 import pandas as pd
 import pytest
 
-from src.match_enzyme import match_enzyme_existence, traverse_enzyme_reaction
+from src.match_enzyme import (match_enzyme_existence, traverse_enzyme_reaction,
+                              reset_enzyme_and_pathway, existence_score_model)
 from src.pathway import enzyme_list, pathway_list
 
 def test_match_enzyme_existence():
@@ -13,10 +14,13 @@ def test_match_enzyme_existence():
     assert all(assert_list)
 
 def test_traverse_enzyme_reaction():
+    reset_enzyme_and_pathway(enzyme_list, pathway_list)
     enzyme_num = [0, 1, 2, 3, 4, 0, 0, 0, 0, 1, 5, 6]
     for i in range(1, 13):
-        enzyme_list[i].set_count(enzyme_num[i-1])
+        if enzyme_num[i-1] != 0:
+            enzyme_list[i].set_count(enzyme_num[i-1])
     traverse_enzyme_reaction(pathway_list[1], enzyme_list, pathway_list)
-    expected = [True, False, True, True, True, True, False, False]
+    expected = [True, False, True, True, True, False, False, False]
     assert_list = [pathway_list[i].visited == expected[i-1] for i in range(1, 9)]
+    assert all(assert_list)
 
