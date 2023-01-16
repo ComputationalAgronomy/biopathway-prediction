5ea50b5c7ae3253b5859d0aeec7b0bddc6c41f75
Zheng-Xiang Ye <65601854+yehzx@users.noreply.github.com>
2023-01-16 14:20:13 +0800

    Add more sequence label rename features

--- src/output_analysis/rename_sequence_label.py
+++ src/output_analysis/rename_sequence_label.py
@@ -4,6 +4,7 @@ species name.
 """
 import glob
 import os
+import re
 import sys
 
 import pandas as pd
@@ -11,6 +12,9 @@ from tqdm import tqdm
 
 # python file path
 DIR = os.path.dirname(__file__)
+
+# non-greedy
+REGEX_SPECIES = re.compile(r'seqhdr=\"(.*?)\"')
 
 input_dir = sys.argv[1]
 input_type = sys.argv[2]
@@ -22,24 +26,43 @@ except Exception:
 
 mapping_dict = {}
 
+
+def handle_weird_filename(str):
+    return str.replace(",", "_").replace(" ", "_").replace(":", "_").replace("/", "_").strip("\n")
+
+
 def get_files(dir, type):
     file_list = glob.glob(os.path.join(dir, f"**/*.{type}"), recursive=True)
     file_list = [file.replace("\\", "/") for file in file_list]
     return file_list
 
 
-def process_label(filename):
+def process_label_fna(filename):
     input_basename = os.path.basename(filename).rsplit(".", 1)[0]
     with open(filename, "r") as f:
         species_name = f.readline().split(" ", 1)[1]
-        species_name = species_name.replace(",", "_").replace(" ", "_").replace(":", "_").replace("/", "_").strip("\n")
+        species_name = handle_weird_filename(species_name)
+    return (input_basename, species_name)
+
+
+def _process_label_prodigal(line):
+    info = line.split(" ", 1)[1]
+    species_name = REGEX_SPECIES.search(info).group(1)
+    return species_name
+
+
+def process_label_prodigal(filename):
+    input_basename = os.path.basename(filename).rsplit(".", 1)[0]
+    with open(filename, "r") as f:
+        species_name = _process_label_prodigal(f.readline())
+        species_name = handle_weird_filename(species_name)
     return (input_basename, species_name)
 
 
 if os.path.isdir(input_dir):
     file_list = get_files(input_dir, input_type)
     for file in file_list:
-        input_basename, species_name = process_label(file)
+        input_basename, species_name = process_label_prodigal(file)
         mapping_dict[input_basename] = species_name
     with open("filename_mapping.csv", "w") as f:
         for input_basename, species_name in mapping_dict.items():
@@ -50,7 +73,7 @@ if os.path.isdir(input_dir):
         for file in get_files(rename_dir, filetype):
             file_dir = os.path.dirname(file)
             basename, ext = os.path.basename(file).rsplit(".", 1)
-            basename = basename.replace(",", "_").replace(" ", "_").replace(":", "_").replace("/", "_").strip("\n")
+            basename = handle_weird_filename(basename)
             try:
                 new_file = os.path.join(file_dir, f"{mapping_dict[basename]}.{ext}")
                 os.rename(file, new_file)
@@ -60,7 +83,7 @@ if os.path.isdir(input_dir):
         for file in file_list:
             file_dir = os.path.dirname(file)
             basename, ext = os.path.basename(file).rsplit(".", 1)
-            basename = basename.replace(",", "_").replace(" ", "_").replace(":", "_").replace("/", "_").strip("\n")
+            basename = handle_weird_filename(basename)
             new_file = os.path.join(file_dir, f"{mapping_dict[basename]}.{ext}")
             os.rename(file, new_file)
 else:
---
+++ src/output_analysis/rename_via_mapping.py
@@ -0,0 +1,47 @@
+"""
+This program will substitute the original downloaded sequence filename by using
+filename_mapping.csv generated from rename_sequence_label. 
+"""
+import glob
+import os
+import sys
+
+import pandas as pd
+from tqdm import tqdm
+
+# python file path
+DIR = os.path.dirname(__file__)
+
+input_dir = sys.argv[1]
+input_type = sys.argv[2]
+mapping_file = sys.argv[3]
+
+mapping_dict = {}
+
+
+def handle_weird_filename(str):
+    return str.replace(",", "_").replace(" ", "_").replace(":", "_").replace("/", "_").strip("\n")
+
+
+def get_files(dir, type):
+    file_list = glob.glob(os.path.join(dir, f"**/*.{type}"), recursive=True)
+    return file_list
+
+
+if os.path.isdir(input_dir):
+    file_list = get_files(input_dir, input_type)
+    with open(mapping_file, "r") as f:
+        for line in f.readlines():
+            input_basename, species_name = line.strip("\n").rsplit(",", 1)
+            mapping_dict[input_basename] = species_name
+    for file in file_list:
+        file_dir = os.path.dirname(file)
+        basename, ext = os.path.basename(file).rsplit(".", 1)
+        basename = handle_weird_filename(basename)
+        new_file = os.path.join(file_dir, f"{mapping_dict[basename]}.{ext}")
+        os.rename(file, new_file)
+else:
+    print("Invalid directory name")
+    sys.exit()
+
+
