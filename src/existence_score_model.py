d07414400705b5f3d7591495335c043778e3843c
Zheng-Xiang Ye <65601854+yehzx@users.noreply.github.com>
2023-02-20 11:37:13 +0800

    Separate the model from match_enzyme into a new file

---
+++ src/existence_score_model.py
@@ -0,0 +1,31 @@
+"""
+==================================================================
+Scoring model used in converting features (eg. identity) to scores 
+==================================================================
+
+Parameters
+----------
+x : numpy.array
+    Features to be converted to scores
+
+Return
+------
+y : numpy.array
+    Scores
+
+"""
+
+import numpy as np
+
+def existence_score_model(x):
+    # prob. ver.1
+    # x = x[x >= 40]
+    # y = 0.18 * np.log(0.15 * (x - 40) + 1) + 0.6
+    # y = np.minimum(y, 1)
+
+    # prob. ver.2
+    # y = 1/(1+np.exp(-1/10 * (x - 40)))
+    
+    # prob. ver.3
+    y = 1/100 * x
+    return y
