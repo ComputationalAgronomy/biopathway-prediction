9f00e0edbec6a4cfea2f35d8846fa38a0bb403bd
Zheng-Xiang Ye <65601854+yehzx@users.noreply.github.com>
2023-02-21 17:00:56 +0800

    Refactor ArgParse in output_analysis

--- output_analysis.py
+++ output_analysis.py
@@ -5,29 +5,52 @@ from src.output_analysis.cytoscape_network_map import cytoscape_format_conversio
 from src.output_analysis.enzyme_mapping_analysis import enzyme_mapping_analysis
 
 
-def main():
-    abs_dir = os.path.dirname(__file__)
+def parent_arguments():
+    parent_parser = argparse.ArgumentParser(description="Parent parser.",
+                                            add_help=False)
+    parent_parser.add_argument("-i", "--input", type=str, required=True,
+                               help="input path")
+    parent_parser.add_argument("-o", "--output", type=str,
+                               default=os.path.dirname(__file__),
+                               help="output path")
+
+    return parent_parser
+
+
+def run_func(args):
+    output_folder = os.path.join(args.output, args.type)
+    os.makedirs(output_folder, exist_ok=True)
+    if args.type == "cytoscape":
+        cytoscape_format_conversion(args.input, output_folder)
+    elif args.type == "mapping_analysis":
+        enzyme_mapping_analysis(args.input, output_folder)
+    else:
+        raise Exception("Unexpected Error!")
+
+
+def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cytoscape", help="[input_path]")
-    parser.add_argument("--mapping_analysis", help="[input_path]")
+    subparser = parser.add_subparsers(help="module name")
+
+    cytoscape_parser = subparser.add_parser(
+        "cytoscape",
+        parents=[parent_arguments()],
+        help="convert the result to cytoscape-readable format"
+    )
+    cytoscape_parser.set_defaults(func=run_func, type="cytoscape")
+
+    mapping_analysis_parser = subparser.add_parser(
+        "mapping_analysis",
+        parents=[parent_arguments()],
+        help="analyze and summarize match_enzyme result"
+    )
+    mapping_analysis_parser.set_defaults(func=run_func, type="mapping_analysis")
 
     args = parser.parse_args()
 
-    output_path = os.path.join(abs_dir, "analysis_output")
-    os.makedirs(output_path, exist_ok=True)
-
-    if args.cytoscape is not None:
-        cytoscape_folder = os.path.join(output_path, "cytoscape")
-        os.makedirs(cytoscape_folder, exist_ok=True)
-        input_path = args.cytoscape
-        cytoscape_format_conversion(input_path, cytoscape_folder)
-
-    if args.mapping_analysis is not None:
-        mapping_analysis_folder = os.path.join(output_path, "mapping_analysis")
-        os.makedirs(mapping_analysis_folder, exist_ok=True)
-        input_path = args.mapping_analysis
-        enzyme_mapping_analysis(input_path, mapping_analysis_folder)
+    return args
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_arguments()
+    args.func(args)
