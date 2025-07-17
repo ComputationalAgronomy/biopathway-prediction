9b3ad06be95d1315775b29bce2fbe388cb33db93
zheng-xiang ye <65601854+yehzx@users.noreply.github.com>
2025-07-17 21:25:18 +0800

    Cleanup

--- biopathpred/cli.py
+++ biopathpred/cli.py
@@ -314,13 +314,6 @@ def optional_arguments(
         description="Optional parser.", add_help=False
     )
     if case == "main":
-        # optional_parser.add_argument(
-        #     "-i",
-        #     "--input",
-        #     type=str,
-        #     required=False,
-        #     help="input a file or directory path",
-        # )
         optional_parser.add_argument("-d", "--database", type=str, help="database path")
         optional_parser.add_argument(
             "-c", "--criteria", type=str, help="selection criteria"
@@ -328,7 +321,9 @@ def optional_arguments(
         optional_parser.add_argument(
             "-f", "--filter", nargs="*", type=str, help="filter options"
         )
-        optional_parser.add_argument( "-m", "--model", type=str, choices=["logistic", "binary"], help="model name")
+        optional_parser.add_argument(
+            "-m", "--model", type=str, choices=["logistic", "binary"], help="model name"
+        )
         optional_parser.add_argument(
             "--verbose", action="store_true", help="print match_enzyme result to screen"
         )
@@ -347,7 +342,9 @@ def optional_arguments(
             "-f", "--filter", nargs="*", type=str, help="filter options"
         )
     elif case == "match_enzyme":
-        optional_parser.add_argument( "-m", "--model", type=str, choices=["logistic", "binary"], help="model name")
+        optional_parser.add_argument(
+            "-m", "--model", type=str, choices=["logistic", "binary"], help="model name"
+        )
         optional_parser.add_argument(
             "--verbose", action="store_true", help="print match_enzyme result to screen"
         )
